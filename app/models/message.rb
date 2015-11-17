d2a8a125ea442d5b44e6a548f06179990770bca1
Raphael Biondi <raf.biondi@gmail.com>
2015-11-17 14:57:38 +0100

    table model added

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,10 @@
 class Message < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :foodtruck
+ belongs_to :user
+ belongs_to :foodtruck
+
+ validates :date, presence: true
+ validates :content, presence: true
+ validates :user_id, presence: true
+ validates :foodtruck_id, presence: true
+
 end
