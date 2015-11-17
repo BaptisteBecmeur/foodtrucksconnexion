a1f275acee6fb0e705ff2382f13b94f17f11f56f
Baptiste Becmeur <baptistebecmeur@gmail.com>
2015-11-17 22:46:23 +0100

    cloudinary

--- app/models/foodtruck.rb
+++ app/models/foodtruck.rb
@@ -7,6 +7,4 @@ class Foodtruck < ActiveRecord::Base
   validates :image, presence: true
   validates :category, inclusion: { in: Foodtruck::CATEGORIES, allow_nil: false }
   validates :user_id, presence: true
-
-
 end
