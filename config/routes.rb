55337a8cd6d93bffeec45c7938a0f7d64ec4f424
Raphael Biondi <raf.biondi@gmail.com>
2015-11-18 14:38:19 +0100

    routes message/reservation

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,8 @@
 Rails.application.routes.draw do
+
   resources :foodtrucks, only: [:index, :show, :new, :create]
-  get 'reservations/index'
-
-  get 'reservations/show'
-
-  get 'reservations/new'
-
-  get 'reservations/create'
-
-  get 'messages/index'
-
-  get 'messages/show'
-
-  get 'messages/new'
-
-  get 'messages/create'
+  resources :messages, only: [:index, :show, :new, :create]
+  resources :reservations, only: [:index, :show, :new, :create]
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'foodtrucks#index'
