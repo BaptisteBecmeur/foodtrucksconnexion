325c6f509f0843931bbacd4038ea76e72be23df5
Baptiste Becmeur <baptistebecmeur@gmail.com>
2015-11-18 21:40:00 +0100

    Added gem geocoder

--- app/models/foodtruck.rb
+++ app/models/foodtruck.rb
@@ -8,6 +8,8 @@ class Foodtruck < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :image, presence: true
   validates :category, inclusion: { in: Foodtruck::CATEGORIES, allow_nil: false }
+  # validates :city, presence: true, uniqueness: true
+  # validates :phone_number, presence: true
   # validates :user_id, presence: true
 
   mount_uploader :image, ImageUploader
