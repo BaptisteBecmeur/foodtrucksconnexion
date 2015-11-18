d7cd6c2ac31565f2a90eab4266ab3e3a57da3a92
Raphael Biondi <raf.biondi@gmail.com>
2015-11-18 11:13:48 +0100

    add a message controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'messages/index'
+
+  get 'messages/show'
+
+  get 'messages/new'
+
+  get 'messages/create'
+
   resources :foodtrucks, only: [:index, :show, :new]
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'foodtrucks#index'
